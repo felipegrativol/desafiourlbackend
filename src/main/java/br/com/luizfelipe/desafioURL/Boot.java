bb9a583515334b0c2e517be4fceb4b5ba9582f65
unknown <carolmotaarchanjo@gmail.com>
2019-07-21 22:35:50 -0300

    Ajuste no CORS

--- src/main/java/br/com/luizfelipe/desafioURL/Boot.java
+++ src/main/java/br/com/luizfelipe/desafioURL/Boot.java
@@ -3,7 +3,6 @@ package br.com.luizfelipe.desafioURL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class Boot
 {
